2d9b77326da85405f5853053069ecc771ddf4205
Gary Ditsch <garyditsch@gmail.com>
2024-07-06 17:15:27 -0400

    mostly styling and intial setup changes to prepare for content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,26 @@
 import { defineCollection, z } from "astro:content";
 
 const postsCollection = defineCollection({
+    type: 'content',
     schema: ({image}) => z.object({
         author: z.string(),
         date: z.string(),
         image: image(),
         title: z.string()
     })
-})
+});
+
+const lessonsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        week: z.string(),
+        title: z.string()
+    })
+});
+
+
 
 export const collections = {
-    posts: postsCollection
-}
+    'posts': postsCollection,
+    'lessons': lessonsCollection,
+};
--- src/content/posts/testing-number-five.md
+++ src/content/posts/testing-number-five.md
@@ -1,10 +1,10 @@
 ---
-author: pam-beesely
+author: dwight-schrute
 categories: ["career advice"]
 date: 08/10/2021
 featured: false
-image: ./images/record.jpg
-title: Creating a successful music brand
+image: ./images/gear.jpg
+title: Testing number 5
 ---
 
 Collaboration is a key component of many creative endeavors, and music production is no exception. In fact, collaboration can be essential to the success of a music project, as it allows for the pooling of diverse talents and perspectives to create something truly unique and compelling.
