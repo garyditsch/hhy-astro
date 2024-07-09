5a596e21e62fb9fe5df1b09b827ca1a2224eadf9
Gary Ditsch <garyditsch@gmail.com>
2024-07-08 22:14:00 -0400

    mobile design, theme, logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), react()]
 });
---
+++ src/components/NavLinks.jsx
@@ -0,0 +1,45 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
+
+const NavLinks = () => {
+    return (
+        <>
+            <a href="/" className="text-dark font-bold">Home</a>
+            <a href="/blog" className="text-dark font-bold">Blog</a>
+            <a href="/about" className="text-dark font-bold">About</a>
+            <a href="/why" className="text-dark font-bold">Why</a>
+            <a href="/lesson" className="text-dark font-bold">Start Today</a>
+        </>
+    )
+}
+
+const Nav = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen)
+    };
+
+    console.log(isOpen)
+
+    return (
+        <>
+        <nav className="flex md:w-1/2 xl:w-1/3 justify-end">
+            <div className="hidden w-full justify-between md:flex">
+                <NavLinks />
+            </div>
+            <div className="md:hidden">
+                <button onClick={toggleNavbar}>{isOpen ? <X /> : <Menu />}</button>
+            </div>
+        </nav>
+        {isOpen && (
+            <div className="flex basis-full flex-col items-center space-y-2">
+                <NavLinks />
+            </div>
+        )}
+        </>
+    );
+};
+
+export default Nav;
