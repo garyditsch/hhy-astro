1bfcaecc0815cee1a7f8794e142830e5e3a4fec5
Gary Ditsch <garyditsch@gmail.com>
2024-07-13 20:53:28 -0400

    have benton and custom palette setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import Nav from "./NavLinks";
 const Header = () => {
 
     return (
-        <header className="bg-light">
-            <div className="bgsticky top-0 z-[1] mx-auto  flex w-full max-w-7xl flex-wrap items-center justify-between p-[1em] font-sans font-bold uppercase backdrop-blur-[100px]">
+        <header className="bg-alice-white">
+            <div className="bgsticky top-0 z-[1] mx-auto flex w-full max-w-7xl flex-wrap items-center justify-between p-[1em] font-sans font-bold uppercase backdrop-blur-[100px]">
                 <Logo />
                 <Nav />
             </div>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ const Logo = () => {
     return (
         <>
             <div className="flex items-center space-x-4">
-                <div className="h-16 w-16">
+                <div className="h-8 w-8">
                     <a href="/"><img src="/LogoSquare.png" alt="logo" /></a>
                 </div>
                 <div>
